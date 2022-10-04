bab0e8efb517dda9c747240711283fd1c4451532
xyx14787008048 <1665916007@qq.com>
2022-10-04 09:09:23 +0000

    002

---
+++ test.dart
@@ -0,0 +1,10 @@
+void core(){
+  var s1 = 'Single quotes work well for string literals.';
+  var s2 = "Double quotes work just as well.";
+  var s3 = 'It\'s easy to escape the string delimiter.';
+  var s4 = "It's even easier to use the other delimiter.";
+}
+
+void main(){
+core();
+}
